b1818557722d84c84ea70b6568a145a1f383e786
Your Name <you@example.com>
2025-01-23 14:41:02 +0500

    bot

---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from handlers import user_handlers
+from service.CbRF import currency
+from config_data import config
+from logging_settings import setup_logging
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Инициализируем бота
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher()
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+
+    # Настраиваем логирование
+    logger = setup_logging()
+    logger.info('Starting bot')
+
+    currency()
+
+    try:
+        # Пропускаем накопившиеся апдейты и запускаем polling
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+
+    except asyncio.CancelledError:
+        logger.info('Polling was cancelled')
+    finally:
+        # Закрываем сессию бота
+        await bot.session.close()
+        logger.info('Bot shutdown')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
